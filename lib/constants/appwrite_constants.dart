f187e4aa304db4ce311e61bbcbef9c76bbd10157
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-03-23 19:17:43 +0530

    added svg

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '641c531c15b3a023a270';
+  static const String projectId = '641c4c2b9017db1d4c13';
+  static const String endPoint = 'https://8080-appwrite-integrationfor-5x4fs2wde15.ws-us92.gitpod.io/v1';
+}
