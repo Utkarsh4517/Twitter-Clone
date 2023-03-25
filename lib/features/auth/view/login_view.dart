4f2e5f68d4e28b816a9f7ef9fadceb0f62235f8b
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-03-25 11:20:49 +0530

    created auth_fields

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/constants/ui_constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -9,11 +10,41 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final appbar = UIConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: UIConstants.appBar(),
+      appBar: appbar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                //textfield1
+                AuthField(controller: emailController, hintText: 'Email address'),
+                //textfield2
+                const SizedBox(height: 25,),
+                AuthField(
+                  controller: passwordController,
+                  hintText: 'Password ',
+                ),
+                //button
+                //textspan
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
