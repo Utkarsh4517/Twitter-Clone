df3edbf705fbec08f33fe3d3d93fd1c2ed913b02
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-03-28 21:36:25 +0530

    made some changes

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/apis/auth_api.dart';
+import 'package:twitter_clone/core/utils.dart';
+
+final authControllerProvider =
+    StateNotifierProvider<AuthController, bool>((ref) {
+  return AuthController(
+    authAPI: ref.watch(authAPIProvider),
+  );
+});
 
 class AuthController extends StateNotifier<bool> {
   final AuthAPI _authAPI; // instance of Auth API
@@ -19,6 +27,6 @@ class AuthController extends StateNotifier<bool> {
       email: email,
       password: password,
     );
-    res.fold((l) => null, (r) => print(r.email));
+    res.fold((l) => showSnackBar(context, l.message), (r) => print(r.email));
   }
 }
--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
-class SignUpView extends StatefulWidget {
+class SignUpView extends ConsumerStatefulWidget { 
+/*ref.read can not be used here therefore we will change the statefulwidget to consumerstatefulwidget and then import the riverpod library
+ now we can use ref.read in onSignUp function
+ */
   static route() => MaterialPageRoute(
         builder: (context) => const SignUpView(),
       );
   const SignUpView({super.key});
 
   @override
-  State<SignUpView> createState() => _SignUpViewState();
+  ConsumerState<SignUpView> createState() => _SignUpViewState();
 }
 
-class _SignUpViewState extends State<SignUpView> {
+class _SignUpViewState extends ConsumerState<SignUpView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -25,6 +30,10 @@ class _SignUpViewState extends State<SignUpView> {
     super.dispose();
     emailController.dispose();
     passwordController.dispose();
+  }
+
+  void onSignUp() {
+  ref.read(authControllerProvider.notifier).signUp(email: emailController.text, password: passwordController.text, context: context); // read is used to call a function from a provider
   }
 
   @override
